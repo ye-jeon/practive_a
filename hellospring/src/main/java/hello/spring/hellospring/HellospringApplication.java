1d17ce141659bd8fb6892f7c87ff8e9be181fbeb
ye-jeon <104572573+ye-jeon@users.noreply.github.com>
2023-01-27 00:44:27 +0900

    Update HellospringApplication.java
    test2

--- hellospring/src/main/java/hello/spring/hellospring/HellospringApplication.java
+++ hellospring/src/main/java/hello/spring/hellospring/HellospringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HellospringApplication {
 
 	public static void main(String[] args) {
-		//git test
+		//git test2
 		SpringApplication.run(HellospringApplication.class, args);
 	}
 
